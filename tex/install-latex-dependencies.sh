1e8c060085604af65b10be2d2305b4099be865af
Cristian MARTINEZ <martinec@scitify.com>
2016-03-29 01:07:27 +0200

    update install-latex-dependencies.sh

--- tex/install-latex-dependencies.sh
+++ tex/install-latex-dependencies.sh
@@ -8,10 +8,12 @@
 # imakeidx
 wget http://mirrors.ctan.org/macros/latex/contrib/imakeidx.zip
 unzip imakeidx.zip
-cd imakeidx
+pushd imakeidx
 latex imakeidx.dtx
 sudo mkdir /usr/share/texmf-texlive/tex/latex/imakeidx
 sudo cp imakeidx.sty /usr/share/texmf-texlive/tex/latex/imakeidx/
+sudo cp imakeidx.sty ..
+popd
 
 # create ls-R databases
 sudo mktexlsr
