fc09c8619a45045d7da2ca3fcb4d7d4a4127686f
Cristian MARTINEZ <martinec@scitify.com>
2016-03-29 01:32:37 +0200

    update install-latex-dependencies.sh

--- tex/install-latex-dependencies.sh
+++ tex/install-latex-dependencies.sh
@@ -3,8 +3,9 @@
 # Unitex/GramLab User's Manual
 # https://github.com/UnitexGramLab/unitex-doc-usermanual
 # =============================================================================
-# This script install latex dependencies
-# based on https://gist.github.com/chrismullins/53b88426d658adf7747b
+# This script install latex dependencies. Download packages based on:
+# https://gist.github.com/chrismullins/53b88426d658adf7747b
+# http://tex.stackexchange.com/revisions/9843c76a-e4b1-4364-8574-80629cd86565
 # =============================================================================
 
 # imakeidx
@@ -22,6 +23,24 @@ cd xpatch
 latex xpatch.ins
 sudo mkdir /usr/share/texmf-texlive/tex/latex/xpatch
 sudo cp xpatch.sty /usr/share/texmf-texlive/tex/latex/xpatch
+
+# l3kernel
+wget http://mirrors.ctan.org/macros/latex/contrib/l3kernel.zip
+unzip l3kernel.zip
+cd l3kernel
+latex l3.ins
+sudo mkdir /usr/share/texmf-texlive/tex/latex/l3kernel
+sudo mkdir /usr/share/texmf-texlive/tex/latex/l3kernel
+sudo cp *.sty /usr/share/texmf-texlive/tex/latex/l3kernel
+sudo cp *.tex /usr/share/texmf-texlive/tex/latex/l3kernel
+sudo cp *.def /usr/share/texmf-texlive/tex/latex/l3kernel
+
+# l3packages
+wget http://mirrors.ctan.org/macros/latex/contrib/l3packages.zip
+cd l3packages
+latex xparse.ins
+sudo mkdir /usr/share/texmf-texlive/tex/latex/xparse
+sudo cp xparse.sty /usr/share/texmf-texlive/tex/latex/xparse
 
 # create ls-R databases
 sudo mktexlsr
