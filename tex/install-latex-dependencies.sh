dcbf139ee61d728940b6fdef2bcef5b9576baa7a
Cristian MARTINEZ <martinec@scitify.com>
2016-03-29 01:22:35 +0200

    update install-latex-dependencies.sh

--- tex/install-latex-dependencies.sh
+++ tex/install-latex-dependencies.sh
@@ -2,7 +2,9 @@
 # =============================================================================
 # Unitex/GramLab User's Manual
 # https://github.com/UnitexGramLab/unitex-doc-usermanual
+# =============================================================================
 # This script install latex dependencies
+# based on https://gist.github.com/chrismullins/53b88426d658adf7747b
 # =============================================================================
 
 # imakeidx
@@ -12,6 +14,14 @@ cd imakeidx
 latex imakeidx.dtx
 sudo mkdir /usr/share/texmf-texlive/tex/latex/imakeidx
 sudo cp imakeidx.sty /usr/share/texmf-texlive/tex/latex/imakeidx/
+
+# xpatch
+wget http://mirrors.ctan.org/macros/latex/contrib/xpatch.zip
+unzip xpatch.zip
+cd xpatch
+latex xpatch.ins
+sudo mkdir /usr/share/texmf-texlive/tex/latex/xpatch
+sudo cp xpatch.sty /usr/share/texmf-texlive/tex/latex/xpatch
 
 # create ls-R databases
 sudo mktexlsr
