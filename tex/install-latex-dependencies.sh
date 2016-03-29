3eb92f8632f975c66baee33fba2616b73f217f5b
Cristian MARTINEZ <martinec@scitify.com>
2016-03-29 02:22:00 +0200

    update install-latex-dependencies.sh

--- tex/install-latex-dependencies.sh
+++ tex/install-latex-dependencies.sh
@@ -5,7 +5,6 @@
 # =============================================================================
 # This script install latex dependencies. Download packages based on:
 # https://gist.github.com/chrismullins/53b88426d658adf7747b
-# http://tex.stackexchange.com/revisions/9843c76a-e4b1-4364-8574-80629cd86565
 # =============================================================================
 
 # imakeidx
@@ -24,15 +23,15 @@ latex xpatch.ins
 sudo mkdir /usr/share/texmf-texlive/tex/latex/xpatch
 sudo cp xpatch.sty /usr/share/texmf-texlive/tex/latex/xpatch
 
-# l3kernel
+# l3kernel TDS
 wget http://mirrors.ctan.org/install/macros/latex/contrib/l3kernel.tds.zip
 unzip l3kernel.tds.zip
-sudo cp l3kernel.tds/tex/latex/l3kernel /usr/share/texmf-texlive/tex/latex/l3kernel
+sudo cp tex/latex/l3kernel /usr/share/texmf-texlive/tex/latex/l3kernel
 
 # l3packages TDS
 wget http://mirrors.ctan.org/install/macros/latex/contrib/l3packages.tds.zip
 unzip l3packages.tds.zip
-sudo cp l3packages.tds/tex/latex/l3packages /usr/share/texmf-texlive/tex/latex/l3packages
+sudo cp tex/latex/l3packages /usr/share/texmf-texlive/tex/latex/l3packages
 
 # create ls-R databases
 sudo texhash
