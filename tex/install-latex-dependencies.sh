fe7cc014057522a534e3c820c95c00f3833dd473
Cristian Martinez <martinec@users.noreply.github.com>
2022-08-12 19:41:21 +0200

    Update install-latex-dependencies.sh

--- tex/install-latex-dependencies.sh
+++ tex/install-latex-dependencies.sh
@@ -7,8 +7,11 @@
 # https://gist.github.com/chrismullins/53b88426d658adf7747b
 # =============================================================================
 
+# options for wget
+WGET_OPTIONS="--no-check-certificate"
+
 # imakeidx
-wget http://mirrors.ctan.org/macros/latex/contrib/imakeidx.zip
+wget ${WGET_OPTIONS} http://mirrors.ctan.org/macros/latex/contrib/imakeidx.zip
 unzip imakeidx.zip
 cd imakeidx
 latex imakeidx.dtx
@@ -16,7 +19,7 @@ sudo mkdir /usr/share/texmf-texlive/tex/latex/imakeidx
 sudo cp imakeidx.sty /usr/share/texmf-texlive/tex/latex/imakeidx/
 
 # xpatch
-wget http://mirrors.ctan.org/macros/latex/contrib/xpatch.zip
+wget ${WGET_OPTIONS} http://mirrors.ctan.org/macros/latex/contrib/xpatch.zip
 unzip xpatch.zip
 cd xpatch
 latex xpatch.ins
@@ -24,12 +27,12 @@ sudo mkdir /usr/share/texmf-texlive/tex/latex/xpatch
 sudo cp xpatch.sty /usr/share/texmf-texlive/tex/latex/xpatch
 
 # l3kernel TDS
-wget http://mirrors.ctan.org/install/macros/latex/contrib/l3kernel.tds.zip
+wget ${WGET_OPTIONS} http://mirrors.ctan.org/install/macros/latex/contrib/l3kernel.tds.zip
 unzip l3kernel.tds.zip
 sudo mv tex/latex/l3kernel /usr/share/texmf-texlive/tex/latex/l3kernel
 
 # l3packages TDS
-wget http://mirrors.ctan.org/install/macros/latex/contrib/l3packages.tds.zip
+wget ${WGET_OPTIONS} http://mirrors.ctan.org/install/macros/latex/contrib/l3packages.tds.zip
 unzip l3packages.tds.zip
 sudo mv tex/latex/l3packages /usr/share/texmf-texlive/tex/latex/l3packages
 
